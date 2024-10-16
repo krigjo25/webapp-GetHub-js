95dd80519ecee320d614ace271296aa273a7d811
Nicolayccxvi <nicolaykblondin@getacademy.no>
2024-10-16 10:55:31 +0000

    loginhandler

--- statics/js/login/loginController.js
+++ statics/js/login/loginController.js
@@ -1,38 +1,10 @@
-let loginController= {
-    handleLogin() {
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
+function loginHandler() {
+    /*
+    this function is responsible for collecting login data
+    from the form and verfifying  it agianst the stored data
+    */
 
-        const user = model.data.users.find(u => u.username === username && u.password === password);
-
-        if (user) {
-            alert("Login successful");
-            modal.app.currentPage = 'index';
-            mainView();
-        } else {
-            alert("incorrect login credentials");
-        }
-        
-    }
-};
-
-//let registerController= {
-  //  handleRegister() {
-   //     const name = document.getElementById('name').value;
-    //    const username = document.getElementById('username').value;
-    //    const password = document.getElementById('password').value;
-   //     const email = document.getElementById('email').value;
-
-   //     model.users.push({
-   //         id: model.data.users.lenght + 1,
-   //         username: username,
-   //         password: password,
-   //         email: email,
-   //         isTeacher: false,
-   //     });
-    //    alert("registration successful");
-   //     model.app.currentPage = 'login';
-   //     mainView(); // redirect to login after registration
-   // }
-//};
-mainView();
+    // collecting data from the form
+    const username = document.querySelector('input[name="username"]').value;
+    const password = document.querySelector('input[name="password"]').value;
+}
