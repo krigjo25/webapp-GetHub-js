d1fdd904eff88b845e0843209192c0bcf6e6ba8d
krigjo25 <krigjo25@gmail.com>
2024-10-16 12:49:19 +0200

    bug fixes

--- statics/js/register/registerView.js
+++ statics/js/register/registerView.js
@@ -6,9 +6,9 @@ function registerView()
     html = /*HTML*/`
             <h1>Register</h1>
             <form>
-                <input type="text" name="Name" oninput'placeholder="Jhon Doe">
-                <input type="password" name="password" placeholder="HammerFest1@!">
-                <input type="text" name="email" placeholder="e.g Jhondoe@gmail.com"> 
+                <input type="text" name="Name" placeholder="Jhon Doe" oninput='modal.inputs.registration.name = this.value'>
+                <input type="password" name="password" placeholder="HammerFest1@!" oninput='modal.inputs.registration.password = this.value'>
+                <input type="text" name="email" placeholder="e.g Jhondoe@gmail.com" oninput='modal.inputs.registration.email = this.value'> 
             </form>
             <button onclick="pushObject()">register</button>`;
 
