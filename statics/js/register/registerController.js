688699424d9a2788d5c59bb9078205e17439144a
krigjo25 <krigjo25@gmail.com>
2024-10-16 12:17:58 +0200

    Initializing a controller function

--- statics/js/register/registerController.js
+++ statics/js/register/registerController.js
@@ -1,3 +1,13 @@
-//  Fetch Courses
-//  Fetch  Milestone
-//  Fetch  feedback
+
+
+function pushObject() 
+
+{
+    /*
+        This function is responsible for collecting data from the form 
+        and pushing it up to the server.
+    */
+
+    //  Collecting data from the form
+    //  Push object up to the server
+}
