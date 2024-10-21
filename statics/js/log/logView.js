a1708d742444af2ad03afb187eb334fd34b44f9e
krigjo25 <krigjo25@gmail.com>
2024-10-21 12:52:55 +0200

    Merge commit fix for logView.js

--- statics/js/log/logView.js
+++ statics/js/log/logView.js
@@ -1,3 +1,23 @@
 function logView(){
   
 }
+// function renderLogs(logs)
+// { 
+// const logList = document.getElementByid(`logList`);
+// logList.innerHTML = ``;
+// logs.forEach(log => {
+//   const li = document.createElement(`li`);
+//   li.textContent = log;
+//   logList.appendChild(li);
+     
+// });}
+// // <-Avsluttende tag
+// function getInput() {
+//     return document.getElementById(`logInput`).value.trim();
+// } //<- Fjerne komma
+//  function clearInput() { // <- Fjerne space
+//      document.getElementById(`logInput`).value = ``;
+    
+//     updateView();}
+//   //  <-Avsluttende tag
+ 
