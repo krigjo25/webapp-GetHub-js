d1cfffdfd1fcdf3440d1915a31ccbd2989684c8a
Nicolayccxvi <nicolaykblondin@getacademy.no>
2024-10-22 11:25:25 +0000

    added some missing script scr files for chorei in index. added comments

--- statics/js/chorei/choreiController.js
+++ statics/js/chorei/choreiController.js
@@ -1,3 +1,21 @@
-//  Fetch Chorei Archive
-//  Fetch  related information
-//  Fetch  Receive user notes
+function navigateToChoreiView(view) {
+    console.log(`Navigating to: ${view}`);
+    model.app.currentpage = view;
+    updateView();
+}
+
+function saveChoreiNotes() {
+    console.log('Saving Chorei notes...');
+    const note1 = document.getElementById('choreiNote1').value;
+    const note2 = document.getElementById('choreiNote2').value;
+    
+    // Save to the model's log
+    model.data.choreiLog.push({
+        time: new Date().toISOString(),
+        note1: note1,
+        note2: note2
+    });
+    
+    // Feedback to the user
+    alert('Dagens Chorei notater er lagret!');
+}
--- statics/js/chorei/choreiView.js
+++ statics/js/chorei/choreiView.js
@@ -1,16 +1,50 @@
-//  Kaller funksjon view
-// skrive funksjon
+function choreiView() {
+    console.log('choreiView is being called');
+    const html = /*HTML*/`
+        <h1>Chorei område:</h1>
+        <div>
+            <button onclick="navigateToChoreiView('chorei')">Chorei</button>
+            <button onclick="navigateToChoreiView('logg')">Logg</button>
+            <button onclick="navigateToChoreiView('moodle')">Moodle</button>
+            <button onclick="navigateToChoreiView('annet')">Annet</button>
+        </div>
+        <div>
+            <h2>Dagens Chorei:</h2>
+            <div>
+                <label for="choreiNote1">Input 1: Område for elev å notere</label>
+                <textarea id="choreiNote1" oninput="model.input.choreiPage.userlog = this.value"></textarea>
+            </div>
+            <div>
+                <label for="choreiNote2">Input 2: Område for elev å notere</label>
+                <textarea id="choreiNote2" oninput="model.input.choreiPage.userlog2 = this.value"></textarea>
+            </div>
+        </div>
+        <div>
+            <h2>Relatert info for i dag:</h2>
+            <ul>
+                <li>Psykologisk trygghet</li>
+                <li>Locus of Control</li>
+                <li>Growth mindset</li>
+                <li>Focused and Diffused brain mode</li>
+                <li>Pomodoro teknikk</li>
+            </ul>
+        </div>
+        <div>
+            <h2>Chorei arkiv:</h2>
+            <table>
+                <tr>
+                    <th>Chorei Nummer</th>
+                    <th>Dato</th>
+                </tr>
+                ${model.data.archive.map(archive => `
+                    <tr>
+                        <td>${archive.name}</td>
+                        <td>${archive.date}</td>
+                    </tr>
+                `).join('')}
+            </table>
+        </div>
+    `;
 
-updateView();
-
-def updateView()
-{
-    // login page view
-        // create login
-
-    // controller
-        // fetch login form
-        // Compile login form with modal data
-            // if true
-                // modal.app.currentpage = "newpage"
+    document.getElementById('app').innerHTML = html;
 }
