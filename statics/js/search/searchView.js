126c09c1acd55ecd22edb0466248918135be2d59
SudhaPuchala <sudhavardhann@gmail.com>
2024-10-28 00:53:33 +0100

    search code

---
+++ statics/js/search/searchView.js
@@ -0,0 +1,17 @@
+updateView()
+    function updateView(){
+        document.getElementById("app").innerHTML = /*HTML*/`
+
+        <input type="text" id="mySearch" onkeyup="myFunction()" placeholder="Search.." title="Type in a category">
+
+<ul id="myMenu">
+  <li><a href="#">About</a></li>
+  <li><a href="#">Chorie</a></li>
+  <li><a href="#">Log</a></li>
+  <li><a href="#">Tools</a></li>
+  
+</ul>
+        
+        
+        `
+    }
