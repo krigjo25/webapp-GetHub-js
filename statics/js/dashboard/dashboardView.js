a36b58f14eb09655fbb88b13f69e728e47a77bbe
krigjo25 <krigjo25@gmail.com>
2024-10-30 12:41:14 +0100

    IC

--- statics/js/dashboard/dashboardView.js
+++ statics/js/dashboard/dashboardView.js
@@ -21,7 +21,7 @@ function dashmain()
     {
         //  Creating an HTML
         html += /*HTML*/`
-            <div onclick="navigateView('${course[i].name}')"> 
+            <div class="card" onclick="navigateView('${course[i].name}')"> 
             <img src="${course[i].img}" alt="${course[i].alt}">
                 <h3>${course[i].name}</h3>
                 <p>${course[i].description}</h3>
