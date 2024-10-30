8d46bf86bd567f4c2db225a672d8d1ab8c041500
SudhaPuchala <sudhavardhann@gmail.com>
2024-10-30 13:38:09 +0100

    Co-authored-by: Kristoffer Gjøsund <krigjo25@users.noreply.github.com> Co-authored-by: krigjo25 <krigjo25@outlook.com> Co-authored-by: martininn <martininn@users.noreply.github.com>

--- statics/js/search/searchView.js
+++ statics/js/search/searchView.js
@@ -18,28 +18,17 @@
                     <th>Mood Log</th>
                     <th>Code Log</th>
                 </tr>
-                ${model.data.users[0].log.map(entry => `
-                    <tr>
-                        <td><b>${entry.title}<b></td>
-                        <td>${entry.time}</td>
-                        <td>${entry.nklog}</td>
-                        <td>${entry.teamlog}</td>
-                        <td>${entry.moodlog}</td>
-                        <td>${entry.codelog}</td>
-                    </tr>
-                `).join('')}
+                
             </table>
         </div>
-    
+    `
     document.getElementById('app').innerHTML = html;
 }
 
-        ` ;
-        updatesearchView()
-    }
+        
+        
 
         
 
      
 
-    
