970cdc772e0cf0a5af5aabba358bfd8c93d39759
SudhaPuchala <sudhavardhann@gmail.com>
2024-10-30 11:25:55 +0100

    Co-authored-by: Kristoffer Gjøsund <krigjo25@users.noreply.github.com>

--- statics/js/search/searchView.js
+++ statics/js/search/searchView.js
@@ -1,14 +1,38 @@
 
 updatesearchView()
     function updatesearchView(){
-        let html = /*HTML*/`
 
-        <h3>Søk</h3>
-        <input
-        type ="text"
-        oninput = "model.inputs.search.text = this.value"
-        value ="";/>
-        <button onclick()>Søk</button>
+         const html = /*HTML*/`
+        <h1>Daglig Logg:</h1>
+        <div>
+            ${navigationMenu()}
+        </div>
+        <div>
+            <h2>Logg Arkiv:</h2>
+            ${updatesearchView()}
+            <table>
+                <tr>
+                    <th>Dato</th>
+                    <th>NøkkelKompetanse</th>
+                    <th>Team Log</th>
+                    <th>Mood Log</th>
+                    <th>Code Log</th>
+                </tr>
+                ${model.data.users[0].log.map(entry => `
+                    <tr>
+                        <td><b>${entry.title}<b></td>
+                        <td>${entry.time}</td>
+                        <td>${entry.nklog}</td>
+                        <td>${entry.teamlog}</td>
+                        <td>${entry.moodlog}</td>
+                        <td>${entry.codelog}</td>
+                    </tr>
+                `).join('')}
+            </table>
+        </div>
+    `;
+    document.getElementById('app').innerHTML = html;
+}
 
         ` ;
     return html;
