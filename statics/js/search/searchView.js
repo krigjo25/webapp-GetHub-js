c881e02a5d039b1db85477eb9ac17176d92cc071
krigjo25 <krigjo25@gmail.com>
2024-11-04 13:38:50 +0100

    Co-authored-by: martininn <martininn@users.noreply.github.com> Co-authored-by: SudhaPuchala <SudhaPuchala@users.noreply.github.com>

--- statics/js/search/searchView.js
+++ statics/js/search/searchView.js
@@ -1,27 +1,10 @@
-
-
 function updatesearchView(){
 
-    const html = /*HTML*/`
-        <h1>Daglig Logg:</h1>
-        <div>
-            ${navigationMenu()}
-        </div>
-        <div>
-            <h2>Logg Arkiv:</h2>
-            <table>
-                <tr>
-                    <th>Dato</th>
-                    <th>NøkkelKompetanse</th>
-                    <th>Team Log</th>
-                    <th>Mood Log</th>
-                    <th>Code Log</th>
-                </tr>
-                
-            </table>
-        </div>
-    `
-    document.getElementById('app').innerHTML = html;
+    // returning the search bar
+    return /*HTML*/`
+        <input type="text" id="search" oninput="model.input.loggPage.search = this.value ">
+        <button onclick="searchLogs()">Søk</button>
+    `;
 }
 
         
