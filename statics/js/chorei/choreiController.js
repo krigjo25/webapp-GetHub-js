0669c76f1934e9fbb608dce0b0b176c9e233fc9f
Nicolay <nicolaykblondin@getacademy.no>
2024-11-05 12:20:14 +0000

    fixed that the log input can be pushed to the user in the model correctly

--- statics/js/chorei/choreiController.js
+++ statics/js/chorei/choreiController.js
@@ -6,18 +6,56 @@ function navigateToChoreiView(view) {
 
 function saveChoreiNotes() {
     console.log('Saving Chorei notes...');
-    const note1 = document.getElementById('choreiNote1').value;
-    const note2 = document.getElementById('choreiNote2').value;
-    
-    // Save to the model's log
-    model.data.choreiLog.push({
-        time: new Date().toISOString(),
-        note1: note1,
-        note2: note2,
-        note3: note3,
-        note4: note4
-    });
-    
+
+    // Retrieve notes from the model input (choreiPage)
+    const mood = model.input.choreiPage.mood;
+    const collaboration = model.input.choreiPage.collabration;
+    const chorei = model.input.choreiPage.chorei;
+    const code = model.input.choreiPage.code;
+    const date = model.input.choreiPage.date;
+
+    // Identify the active user using session ID
+    const userId = model.session.id;
+    const user = model.data.users.find(u => u.id === userId);
+
+    if (!user) {
+        console.error('User not found or not logged in');
+        alert('User not found or not logged in');
+        return;
+    }
+
+    // Create a new log entry
+    const newLogEntry = {
+        date: date,
+        nklog: chorei,
+        teamlog: collaboration,
+        moodlog: mood,
+        codelog: code
+    };
+
+    // Add the entry to the user's log
+    user.log.push(newLogEntry);
+
     // Feedback to the user
     alert('Dagens Chorei notater er lagret!');
+    console.log('Chorei notes saved for user:', user.username, user.log);
+
+    // Clear the input fields in the model after saving
+    model.input.choreiPage.mood = "";
+    model.input.choreiPage.collabration = "";
+    model.input.choreiPage.chorei = "";
+    model.input.choreiPage.code = "";
+
+    // Refresh the view
+    updateView();
 }
+
+function updateView() {
+    if (model.app.currentpage === 'chorei') {
+        choreiView();
+    }
+}
+
+window.onload = () => {
+    updateView();
+};
