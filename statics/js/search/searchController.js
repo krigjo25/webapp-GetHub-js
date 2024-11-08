b2d2a75c114e864416592fd93949d92bc3b54a51
Nicolay <nicolaykblondin@getacademy.no>
2024-11-08 10:32:28 +0000

    made the search function not casesensetive so its more usefull for the user

--- statics/js/search/searchController.js
+++ statics/js/search/searchController.js
@@ -1,83 +1,67 @@
 function searchLogs() {
 
-  //  Initialize user id, search bar text
+  // Initialize user id, search bar text
   let id = model.session.id;
 
-  //  peke mot ->  loggPage.search ->
+  // peke mot -> loggPage.search ->
   const search = model.input.loggPage.search;
 
-  //  Endre den her til variablen som peker mot den teksten
-  if(search != '')
-  {
-    search.toLowerCase();
-    //  Set the current page to search page
+  // Ensure search term is not empty
+  if (search !== '') {
+    // Convert search text to lowercase
+    const searchLower = search.toLowerCase();
+    
+    // Set the current page to search page
     model.app.currentpage = 'search';
 
-    //  Filter the logs
-    model.search = filterPeople(search, id);;
+    // Filter the logs with case-insensitive search
+    model.search = filterPeople(searchLower, id);
   }
 
-  // Initialize the search result to the model
-  
-
-  //  Update the page view
+  // Update the page view
   updateView();
 }
 
-function filterPeople(search,id) {
+function filterPeople(search, id) {
 
-  //  Initializing an filtered array
+  // Initializing a filtered array
   let filteredLogs = [];
 
-  //  Initializing a db users array
+  // Initializing a db users array
   let users = model.data.users;
 
-  // linear algorithm
-  for(let i = 0;i < users.length; i++){
+  // Linear algorithm
+  for (let i = 0; i < users.length; i++) {
 
-    //  Ensure that the user is equal to the selected id
-    if (users[i].id === id)
-      {
+    // Ensure that the user is equal to the selected id
+    if (users[i].id === id) {
 
-      //  Create a variable and assign specefic user log
-        let word = users[i].log;
+      // Create a variable and assign specific user log
+      let logs = users[i].log;
 
-        //  2d linear algorithm
-        for (let j=0; j < word.length; j++)
-        {
-          //  Ensure that logs includes the searched item .includes(search)
-          if (word[j].date.includes(search))
-          {
-            filteredLogs.push(word[j]);
-
-          }
-          else if(word[j].title.includes(search))
-          {
-            filteredLogs.push(word[j]);
-          }
-          else if(word[j].nklog.includes(search))
-          {
-            filteredLogs.push(word[j]);
-          }
-          else if(word[j].teamlog.includes(search))
-          {
-            filteredLogs.push(word[j]);
-          }
-          else if(word[j].moodlog.includes(search))
-          {
-            filteredLogs.push(word[j]);
-          }
-          else if(word[j].codelog.includes(search))
-          {
-            filteredLogs.push(word[j]);
-          }
+      // 2D linear algorithm
+      for (let j = 0; j < logs.length; j++) {
+        // Ensure that logs include the searched item in a case-insensitive way
+        if (logs[j].date.toLowerCase().includes(search)) {
+          filteredLogs.push(logs[j]);
         }
+        else if (logs[j].title.toLowerCase().includes(search)) {
+          filteredLogs.push(logs[j]);
+        }
+        else if (logs[j].nklog.toLowerCase().includes(search)) {
+          filteredLogs.push(logs[j]);
+        }
+        else if (logs[j].teamlog.toLowerCase().includes(search)) {
+          filteredLogs.push(logs[j]);
+        }
+        else if (logs[j].moodlog.toLowerCase().includes(search)) {
+          filteredLogs.push(logs[j]);
+        }
+        else if (logs[j].codelog.toLowerCase().includes(search)) {
+          filteredLogs.push(logs[j]);
+        }
+      }
     }
-    return filteredLogs;
-
+  }
+  return filteredLogs;
 }
-}
-
-
-
-
