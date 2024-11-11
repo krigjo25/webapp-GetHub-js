9d5ac3cf00d0992098928daf93b18de74918523c
krigjo25 <krigjo25@gmail.com>
2024-11-11 20:02:27 +0100

    Added id's for inputs and labels

--- statics/js/register/registerView.js
+++ statics/js/register/registerView.js
@@ -6,10 +6,14 @@ function registerView() {
         <div class="register-view">
         <h1>Register</h1>
         <form onsubmit='pushObject(event)'>
-            <input type="text" name="Name" placeholder="e.g John Doe" oninput='model.input.registration.name = this.value'>
-            <input type="text" name="username" placeholder="e.g johnny123" oninput='model.input.registration.username = this.value'>
-            <input type="password" name="password" placeholder="e.g HammerFest1@!" oninput='model.input.registration.password = this.value'>
-            <input type="text" name="email" placeholder="e.g johndoe@gmail.com" oninput='model.input.registration.email = this.value'> 
+            <label for="Name">Name:</label>
+            <input id="name" type="text" name="Name" placeholder="e.g John Doe" oninput='model.input.registration.name = this.value'>
+            <label for="username">Username:</label>
+            <input id="username" type="text" name="username" placeholder="e.g johnny123" oninput='model.input.registration.username = this.value'>
+            <label for="password">Password:</label>
+            <input id="password" type="password" name="password" placeholder="e.g HammerFest1@!" oninput='model.input.registration.password = this.value'>
+            <label for="e-mail">E-mail:</label>
+            <input id="e-mail" type="text" name="email" placeholder="e.g johndoe@gmail.com" oninput='model.input.registration.email = this.value'> 
             <input type="submit" value="Register">
         </form>`;
 
